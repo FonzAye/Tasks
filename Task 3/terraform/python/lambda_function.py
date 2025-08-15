1742d0a660c29f83d6885f105ad2ac03d5460a22
FonzAye <george_super@ukr.net>
2025-08-15 09:26:58 +0300

    fixed python

--- Task 3/terraform/python/lambda_function.py
+++ Task 3/terraform/python/lambda_function.py
@@ -1,5 +1,25 @@
+# health_check.py
+import requests
+
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hello from Python Lambda!"
-    }
+    url = 'https://32b2c93fff1e.ngrok-free.app/'
+    
+    try:
+        response = requests.get(url, timeout=5)
+        status_code = response.status_code
+        response_time = response.elapsed.total_seconds()
+        
+        if 200 <= status_code <= 399:
+            message = f'Status code: {status_code}, Response time: {response_time:.2f}s'
+        elif 400 <= status_code <= 599:
+            message = f'We are in trouble Houston! Status code: {status_code}'
+        else:
+            message = f'Unexpected status code: {status_code}'
+            
+        print(message)
+        return {"statusCode": status_code, "message": message}
+
+    except requests.exceptions.RequestException as e:
+        error_message = f'Request failed: {e}'
+        print(error_message)
+        return {"statusCode": 500, "message": error_message}
--- Task 3/terraform/python/test.py
+++ Task 3/terraform/python/test.py
@@ -3,13 +3,15 @@ import requests
 import schedule
 import time
 
+url = 'https://68998eb18d7e.ngrok-free.app/'
+
 def fetch_url():
     try:
-        response = requests.get('https://b47f85463dd9.ngrok-free.app')
+        response = requests.get(url)
         status_code = response.status_code
         response_time = response.elapsed.total_seconds()
         
-        if status_code >= 200 or status_code <= 399:
+        if status_code >= 200 and status_code <= 399:
             print(f'The status code: {status_code}, Response time: {response_time}')
         elif status_code >= 400 and status_code <= 599:
             print(f'We are in trouble Houston! Status code: {status_code}')
@@ -19,7 +21,7 @@ def fetch_url():
     except requests.exceptions.RequestException as e:
         print(f'Request failed: {e}')
 
-schedule.every(5).seconds.do(fetch_url)
+schedule.every(2).seconds.do(fetch_url)
 
 while True:
     schedule.run_pending()
